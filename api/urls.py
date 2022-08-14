30fd07e04eff5fe7f68e69bbe04a6ff06e9eea98
piayer69 <maxmarsz1@gmail.com>
2022-08-14 13:32:49 +0200

    Added APIView for refresh token blacklisting

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import ListNotes, UserRegister
+from .views import ListNotes, UserRegister, BlacklistTokenView
 
 urlpatterns = [
     path('notes/', ListNotes.as_view({'get': 'list', 'post': 'create'}), name='notes-list'),
     path('notes/<int:pk>/', ListNotes.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}), name='note'),
     path('register/', UserRegister.as_view(), name='user_registrations'),
+    path('logout/', BlacklistTokenView.as_view(), name='token_blacklist'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from django.contrib.auth.models import User
 
@@ -12,9 +14,12 @@ from .serializers import NoteSerializer, UserSerializer
 class ListNotes(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class UserRegister(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         if User.objects.filter(username=request.data['username']).first(): return Response(status=status.HTTP_400_BAD_REQUEST, data={'Error': 'Username already taken'})
         if User.objects.filter(email=request.data['email']).first() is not None: return Response(status=status.HTTP_400_BAD_REQUEST, data={'Error': 'Email already taken'})
@@ -25,3 +30,17 @@ class UserRegister(APIView):
         serialized = UserSerializer(user)
 
         return Response(serialized.data)
+
+
+class BlacklistTokenView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'code': 'Success'})
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
